525c52567ea9a26777792fc49f05935e8fdee79a
Cody Ashe-McNalley <codyam@ucla.edu>
2015-10-06 16:13:33 -0700

    Production ETL

--- Quarantine/Uploads/ETL.sh
+++ Quarantine/Uploads/ETL.sh
@@ -3,21 +3,36 @@
 shopt -s extglob
 
 
-UploadID=$(mysql "CALL pollUploads();")
-
+UploadID=$(mysql --batch --silent Quarantine -e "CALL pollUploads();")
 [ $UploadID -le 0 ] && exit
 
-UploadFile=$(ls -1 /data/UPLOADS/$UploadID/image_file)
+rsync --recursive --times --update --progress --chmod=u+rwx,g+rwx,o-rwx webserver::uploads/$UploadID/image_file /data/UPLOADS/$UploadID/
 
-QuarantineDir=/data/QUARANTINE/$UploadID/image_file/${UploadFile}_extracted
+UploadDir=/data/UPLOADS/$UploadID/image_file
+[ -d $UploadDir ] || exit
+
+UploadFile=( $(ls -1 $UploadDir) )
+[ ${#UploadFile[@]} -gt 1 ] && exit
+UploadFileType=$(file --brief --mime-type --preserve-date $UploadDir/$UploadFile)
+
+QuarantineDir=${UploadDir/UPLOADS/QUARANTINE}/${UploadFile}_extracted
 [ -d $QuarantineDir ] && exit
 
 mkdir -pv $QuarantineDir
-pushd $QuarantineDir
-FileType=$(file $UploadFile)
+
+case $UploadFileType in 
+	application/zip) unzip $UploadDir/$UploadFile -d $QuarantineDir ;;
+	application/x-gzip) tar -C $QuarantineDir -xzvf $UploadDir/$UploadFile ;;
+	application/octet-stream) exit ;;
+esac
 
 
-NetworkID=$(mysql "getNetworkID()");
+NetworkID=$(mysql --batch --silent Quarantine -e "CALL getNetworkID($UploadID)");
 case $NetworkID in
-	2) find $QuarantineDir -type f | push+fix 0 {};;
+	3)	[[ $UploadFile =~ ([0123456]{4})_?([0-9]{6}).* ]] || exit
+		[ ${#BASH_REMATCH[1]} -eq 4 ] || exit
+		[ ${#BASH_REMATCH[2]} -eq 6 ] || exit
+		PatientID=${BASH_REMATCH[1]}${BASH_REMATCH[2]}
+		find $QuarantineDir -type f | push+fix $UploadID {} $PatientID
+		;;
 esac
