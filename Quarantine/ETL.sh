664fd43873e84fa648b664797c992fa5ee5dea13
cody-a-m <codyam@ucla.edu>
2016-03-23 14:51:21 -0700

    Catching up on repo code

--- Quarantine/ETL.sh
+++ Quarantine/ETL.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-
 shopt -s extglob
+export PATH=/usr/local/bin:$PATH
 
 
 UploadID=$(mysql --batch --silent Quarantine -e "CALL pollUploads();")
 [ $UploadID -le 0 ] && exit
 
-rsync --recursive --times --update --progress --chmod=u+rwx,g+rwx,o-rwx webserver::uploads/$UploadID/image_file /data/UPLOADS/$UploadID/
+rsync --recursive --times --update --progress --chmod=u+rwx,g+rwx,o-rwx --rsh="ssh -p2222 -lBoFH" webserver:/srv/www/upload/pain/$UploadID/ /data/UPLOADS/$UploadID/
 
-UploadDir=/data/UPLOADS/$UploadID/image_file
+UploadDir=$(ls -1d /data/UPLOADS/$UploadID/image_file*)
 [ -d $UploadDir ] || exit
 
 UploadFile=( $(ls -1 $UploadDir) )
@@ -19,10 +19,13 @@ QuarantineDir=${UploadDir/UPLOADS/QUARANTINE}/${UploadFile}_extracted
 [ -d $QuarantineDir ] || mkdir -pv $QuarantineDir
 
 case $UploadFileType in 
+	application/x-tar) tar -C  $QuarantineDir -xvf $UploadDir/$UploadFile ;;
 	application/zip) unzip $UploadDir/$UploadFile -d $QuarantineDir ;;
 	application/x-gzip) tar -C $QuarantineDir -xzvf $UploadDir/$UploadFile ;;
 	application/octet-stream) exit ;;
 esac
+chgrp -R 2000 $QuarantineDir
+chmod -R 750 $QuarantineDir
 
 
 NetworkID=$(mysql --batch --silent Quarantine -e "CALL getNetworkID($UploadID)");
@@ -31,6 +34,12 @@ case $NetworkID in
 		[ ${#BASH_REMATCH[1]} -eq 4 ] || exit
 		[ ${#BASH_REMATCH[2]} -eq 6 ] || exit
 		PatientID=${BASH_REMATCH[1]}${BASH_REMATCH[2]}
-		find $QuarantineDir -type f | parallel --no-notice --jobs /tmp/procfile ~/cnsdatacore/Quarantine/Uploads/push+fix.sh $UploadID {} $PatientID
+		#find $QuarantineDir -type f | parallel --no-notice --jobs /tmp/procfile ~/cnsdatacore/Quarantine/push+fix.sh $UploadID {} $PatientID
+		repo_push_dir -u $UploadID -d "$QuarantineDir" -s $PatientID
+		;;
+	4)	[[ $UploadFile =~ [0-9]{4}[A-Za-z] ]] || exit
+		[ ${#BASH_REMATCH} -eq 5 ] || exit
+		PatientID=${BASH_REMATCH}
+		repo_push_dir -u $UploadID -d "$QuarantineDir" -s $PatientID
 		;;
 esac
