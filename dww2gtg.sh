ff821471409033b76d7f681dd39b425f242e5304
cody-a-m <codyam@ucla.edu>
2017-03-17 00:21:11 -0700

    Updating DWW linking

--- dww2gtg.sh
+++ dww2gtg.sh
@@ -2,6 +2,7 @@ sid=$1
 s=$(mysql -Brs ImagingRepositoryV6 -e "select concat_ws('_',SubjectName,replace(ScanDate,'-','')) from StudyImageInfo where StudyImageID=${sid};")
 #[ ${#s} -eq 0 -a -e $d/count_0.txt ] && echo "$d bad" && continue
 #[ -d  /data/GTG/metabolon/data/$s ] && exit
-mkdir -pv  /data/GTG/DWW/data/$s/connectivity/AggFibersScript_1.OutputDirectory-1
-[ -L /data/GTG/DWW/data/$s/connectivity/AggFibersScript_1.OutputDirectory-1/count_0.txt ] && exit
-ln -sv /data/DWW/$sid/count_0.txt /data/GTG/DWW/data/$s/connectivity/AggFibersScript_1.OutputDirectory-1/
+d=/data/GTG/DWW/data/$s/connectivity/AggFibersScript_1.OutputDirectory-1
+[ -L $d/count_0.txt ] && exit
+mkdir -pv  $d
+ln -sv /data/DWW/$sid/count_0.txt $d/count_0.txt
